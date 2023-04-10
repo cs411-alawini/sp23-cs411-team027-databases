b94cedabb74c4e8fe8e2f9db4e44c4ca8b2df26c
kaanyigit-repo <kyigit2@illinois.edu>
2023-04-10 14:04:03 -0500

    front end changes

--- frontend/src/Components/Profile/Profile.jsx
+++ frontend/src/Components/Profile/Profile.jsx
@@ -1,6 +1,12 @@
 
-export default function Profile() {
+export default function Profile(props) {
+    console.log(props)
+    // <tbody>
+    //   {items.map(item => <ObjectRow key={item.id} name={item.name} />)} 
+    // </tbody>
     return (
-        <div>Profile</div>
+      <div>Profile
+     
+      </div>
     )
 }
