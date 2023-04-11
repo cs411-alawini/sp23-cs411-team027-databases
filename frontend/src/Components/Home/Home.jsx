5dbea0588f41bdb66914976f34437a213b633b23
kaanyigit-repo <kyigit2@illinois.edu>
2023-04-10 23:36:26 -0500

    added profile page and querry basic for further use

--- frontend/src/Components/Home/Home.jsx
+++ frontend/src/Components/Home/Home.jsx
@@ -1,15 +1,16 @@
-import {Box,SimpleGrid} from "@chakra-ui/react"
+import {Box,Button,SimpleGrid} from "@chakra-ui/react"
+import { Link } from "react-router-dom";
 
 export default function Home(props) {
-    console.log(props)
-    // <tbody>
-    //   {items.map(item => <ObjectRow key={item.id} name={item.name} />)} 
-    // </tbody>
+  
+  
     return (
       <div>Action Games
       <SimpleGrid columns={4} spacing={10} minChildWidth={250} padding={15}>
-        {props.actionGames.map(item => <Box bg="white" height="200px" border="1px solid" >{item.GameName}</Box>)}
+        {props.actionGames.map(item => <Link bg="white" height="200px" border="1px solid" to="/profile" state={{ name: item.GameName }} >{item.GameName}</Link>)}
       </SimpleGrid>
       </div>
     )
+
+    
 }
--- frontend/src/Components/Profile/Profile.jsx
+++ frontend/src/Components/Profile/Profile.jsx
@@ -1,45 +1,41 @@
 import React from 'react';
+import axios from 'axios'
+import {useState, useEffect, Box, SimpleGrid, Button} from "react"
+
 import "./Profile.css";
+import { useLocation } from 'react-router-dom'
 
 import logo from '/Users/kaanyigit/Documents/sp23-cs411-team027-databases/frontend/src/logo.svg'; // Tell webpack this JS file uses this image
 
 
+
 export default function Profile(props) {
     console.log(props)
-    console.log(logo); // /logo.84287d09.png
-
-    // <tbody>
-    //   {items.map(item => <ObjectRow key={item.id} name={item.name} />)} 
-    // </tbody>
-
     
-
-
-
+    const location = useLocation()
+    const {name}  = location.state
+   
     return (
-      <div>
-        <div className="container">
-      <img src={logo} alt="Centered" style={{ transform: 'scale(0.5)' }}/>
-    </div>
-    <div className="kant">
-        <h1>name Of the Game</h1>
       
-    </div>
-    <div className="kant">
-        <h1>Rating: 4/5 </h1>
-    </div>
-    <div className="kant">
-        <h1>Genre: Sci-Fi </h1>
-    </div>
-    <div className="paragraphy">
-        <p1>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vestibulum sapien sit amet risus pretium, nec pulvinar tortor scelerisque. Interdum et malesuada fames ac ante ipsum primis in faucibus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nunc eget nulla non mi fringilla volutpat. Donec in vestibulum erat. Maecenas non lorem id leo rutrum tempor. Sed vestibulum eros sit amet aliquam convallis.
-
-</p1>
-    </div>
-
-
-
+      
+      <div>
         
+        <div className="container">
+        <img src={logo} alt="Centered" style={{ transform: 'scale(0.5)' }}/>
+        </div>
+              <div className="kant">
+                <h1>{name} </h1>
+              </div>
+              <div className="kant">
+                  <h1>Rating: {} /5</h1>
+              </div>
+              <div className="kant">
+                  <h1>Genre: {} </h1>
+              </div>
+            <div className="paragraphy">
+                <p1>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vestibulum sapien sit amet risus pretium, nec pulvinar tortor scelerisque. Interdum et malesuada fames ac ante ipsum primis in faucibus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nunc eget nulla non mi fringilla volutpat. Donec in vestibulum erat. Maecenas non lorem id leo rutrum tempor. Sed vestibulum eros sit amet aliquam convallis.</p1>
+            </div>
+
 
       
       </div>
